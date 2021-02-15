18e807e8d8ea9040d167097965e43ee0638f5128
AloysiusAji <73976886+AloysiusAji@users.noreply.github.com>
2021-02-15 12:34:58 +0700

    minggu-2-71180298

---
+++ soal 2.1.py
@@ -0,0 +1,12 @@
+'''Menghitung rata-rata dari 3 bilangan bulat sembarang.'''
+
+print("Menghitung rata-rata dari 3 bilangan bulat sembarang")
+print("ex: 1,2,3,4,5")
+
+bil1 = int(input("Masukan bilangan bulat ke-1: "))
+bil2 = int(input("Masukan bilangan bulat ke-2: "))
+bil3 = int(input("Masukan bilangan bulat ke-3: "))
+
+mean = (bil1 + bil2 + bil3) / 3
+
+print("Hasil rata-rata 3 bilangan bulat adalah: ",mean)
